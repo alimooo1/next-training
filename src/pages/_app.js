52f44faa1b4e13c99b075b29a3d7fba50d6d56e4
Ali Moallemi <Alimoallemi@gmail.com>
2023-03-31 15:58:17 +0330

    font added

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,12 @@
 import "@/styles/globals.css";
+import { Dosis } from "next/font/google";
+
+const font = Dosis({ subsets: ["latin"], weight: ["300", "400", "700"] });
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <main className={font.className}>
+      <Component {...pageProps} />;
+    </main>
+  );
 }
