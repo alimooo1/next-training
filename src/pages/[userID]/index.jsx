bb7430bd63f6d074cb83ac8fa027724c23ec1089
Ali Moallemi <alimoallemi30@gmail.com>
2023-04-02 15:46:01 +0330

    added getStaticPaths for SingleUser

--- src/pages/[userID]/index.jsx
+++ src/pages/[userID]/index.jsx
@@ -12,9 +12,28 @@ export default index;
 
 export async function getStaticProps(context) {
   const { params } = context;
-  console.log(params);
   const data = await fetch(`https://reqres.in/api/users/${params.userID}`);
   const result = await data.json();
   const user = result.data;
-  return user;
+  console.log(user);
+
+  return {
+    props: {
+      userData: user,
+    },
+  };
+}
+
+export async function getStaticPaths() {
+  return {
+    paths: [
+      { params: { userID: "1" } },
+      { params: { userID: "2" } },
+      { params: { userID: "3" } },
+      { params: { userID: "4" } },
+      { params: { userID: "5" } },
+      { params: { userID: "6" } },
+    ],
+    fallback: false,
+  };
 }
